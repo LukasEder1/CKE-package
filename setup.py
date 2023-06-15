0825a42c9af98a18381dd6775ae0f27ee3d4f41f
Lukas Eder <40723119+LukasEder1@users.noreply.github.com>
2023-06-15 18:56:22 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from codecs import open
+from setuptools import setup, find_packages
+import os
+
+
+reqs_path = 'requirements.txt'
+reqs =  []
+
+if os.path.isfile(reqs_path ):
+  with open(reqs_path) as file:
+    reqs = file.read().splitlines()
+
+with open("READEME.md", "r", encoding="utf-8") as file:
+  ld = f.read()
+  
+ 
+  
+setuptools.setup(
+    name='cke',
+    version='0.1',
+    author='Lukas Eder',
+    author_email='lukaseder.el@protonmail.com',
+    description='Contrastive Keyword Extraction',
+    long_description=ld,
+    url='https://github.com/LukasEder1/CKE-package',
+    project_urls = {
+        "Bug Tracker": "https://github.com/LukasEder1/CKE-package/issues"
+    },
+    license='MIT',
+    packages=['cke'],
+    include_package_data=True,
+    install_requires=reqs,
+)
