6c3a84bc7ae1366968efd4fc42563f3ce487e7d9
Lukas Eder <40723119+LukasEder1@users.noreply.github.com>
2023-06-15 20:11:19 +0200

    Update __init__.py

--- cke/__init__.py
+++ cke/__init__.py
@@ -1,4 +1,5 @@
-from .contrastive_keyword_extraction import extract_contrastive_keywords
-
+import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
+
+from .contrastive_keyword_extraction import extract_contrastive_keywords
