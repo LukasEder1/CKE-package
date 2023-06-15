8113a953b7927943df79103a03e42a9aba0a4a0c
Lukas Eder <40723119+LukasEder1@users.noreply.github.com>
2023-06-15 19:52:45 +0200

    Update sentence_importance.py

--- cke/sentence_importance.py
+++ cke/sentence_importance.py
@@ -8,7 +8,6 @@ import nltk
 from sentence_transformers import SentenceTransformer, util
 import re
 from yake.highlight import TextHighlighter
-import keyword_extraction
 import math
 import pysbd
 import numpy
@@ -16,6 +15,22 @@ import numpy
 import nltk
 
 from sumy.summarizers import AbstractSummarizer
+
+import yake
+
+def extract_yake(documents, language="en", max_ngram_size=1, 
+                 deduplication_threshold = 0.9, deduplication_algo = 'seqm',
+                 windowSize=1,numOfKeywords=10):
+    number_of_documents = len(documents)
+    kws = {version:[] for version in range(number_of_documents)}
+    custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, 
+                                                dedupLim=deduplication_threshold,dedupFunc=deduplication_algo, 
+                                                windowsSize=windowSize, top=numOfKeywords, features=None)
+    
+    for current in range(number_of_documents):
+        kws[current] = custom_kw_extractor.extract_keywords(documents[current])
+            
+    return kws
 
 
 class TextRankSummarizer(AbstractSummarizer):
