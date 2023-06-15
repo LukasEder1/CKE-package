78ea7d46c710dcce2898dd349fbccd8ffda3a864
Lukas Eder <40723119+LukasEder1@users.noreply.github.com>
2023-06-15 19:07:22 +0200

    Update __init__.py

--- cke/__init__.py
+++ cke/__init__.py
@@ -1,3 +1,1 @@
-from .contrastive_keyword_extraction import extract_contrastive_keywords
-from .sentence_comparision import *
-from .sentence_importance import *
+from .utilities import alpha_combination
